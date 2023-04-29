a990a5c965f00d5ebb84a1247b36d789be24a735
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2023-04-29 19:29:33 +0100

    Update core.c

--- X16Emulator/EmulatorCoreLinux/core.c
+++ X16Emulator/EmulatorCoreLinux/core.c
@@ -2,14 +2,14 @@
 
 extern int64_t asm_func(void *state);
 
-int64_t fmEmulatorCode(void* state)
+int32_t fmEmulatorCode(void* state)
 {
-    int64_t toReturn = 0;
+    int32_t toReturn = 0;
 
     __asm__ __volatile__(
         "mov %%rdi, %%rsi \t\n"
         "call asm_fund    \t\n"
-        "mov %%rax, %0    \t\n" : "=a"(toReturn) : :);
+        "mov %%eax, %0    \t\n" : "=a"(toReturn) : :);
 
     return toReturn;
 }

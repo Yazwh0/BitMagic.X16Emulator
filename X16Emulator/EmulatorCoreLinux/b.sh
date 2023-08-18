8a2605e512287b22f8de85b02d472d0f60133625
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2023-08-18 15:38:55 +0100

    Fix build

---
+++ X16Emulator/EmulatorCoreLinux/b.sh
@@ -0,0 +1,2 @@
+./build.sh
+cp EmulatorCore.so /home/bjury/.config/Code/User/globalStorage/yazwh0.bitmagic/file-downloader-downloads/X16D/EmulatorCore
--- X16Emulator/EmulatorCoreLinux/build.sh
+++ X16Emulator/EmulatorCoreLinux/build.sh
@@ -1,2 +1,1 @@
 gcc -shared -fno-pie -o EmulatorCore.so -fPIC core.c core.obj -Wall -g -m64
-cp EmulatorCore.so /home/bjury/.config/Code/User/globalStorage/yazwh0.bitmagic/file-downloader-downloads/X16D/EmulatorCore

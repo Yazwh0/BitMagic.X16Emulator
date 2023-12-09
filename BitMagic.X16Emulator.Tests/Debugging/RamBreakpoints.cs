1da56cf8c871f6192316e1602a20f9a667c28e44
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2023-12-09 11:47:34 +0000

    Add unit test

--- BitMagic.X16Emulator.Tests/Debugging/RamBreakpoints.cs
+++ BitMagic.X16Emulator.Tests/Debugging/RamBreakpoints.cs
@@ -52,6 +52,33 @@ public class RamBreakpoints
     }
 
     [TestMethod]
+    public async Task BankedRam2()
+    {
+        var emulator = new Emulator();
+
+        emulator.Breakpoints[0x10000 + (0x2000 * 2) + 0x811] = 1; // 0x811 in ram bank 2
+
+        await X16TestHelper.Emulate(@"
+                .machine CommanderX16R40
+                .org $810
+                lda #2
+                sta RAM_BANK
+                
+                lda #$ea ; nop
+                sta $a810
+                sta $a811
+                sta $a812
+                lda #$db
+                sta $a813
+
+                jmp $a810
+                stp",
+                emulator, expectedResult: Emulator.EmulatorResult.Breakpoint);
+
+        emulator.AssertState(Pc: 0xa811);
+    }
+
+    [TestMethod]
     public async Task BankedRom()
     {
         var emulator = new Emulator();

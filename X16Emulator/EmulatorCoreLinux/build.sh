98beac4ee0dbb575bcc9fe972972dbe1bc9fa2f8
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2025-01-18 11:51:42 +0000

    Add version to gcc

--- X16Emulator/EmulatorCoreLinux/build.sh
+++ X16Emulator/EmulatorCoreLinux/build.sh
@@ -1,1 +1,1 @@
-gcc -shared -fno-pie -o EmulatorCore.so -fPIC core.cpp core.obj -Wall -g -m64 -z noexecstack
+gcc -shared -fno-pie -o EmulatorCore.so -fPIC core.cpp core.obj -Wall -g -m64 -z noexecstack --version

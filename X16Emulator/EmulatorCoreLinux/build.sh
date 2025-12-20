4ad02922d66a56f179547a44d59d304824b3552a
Yazwh0 <90315107+Yazwh0@users.noreply.github.com>
2025-12-20 13:44:05 +0000

    Change build to exclude debug info

--- X16Emulator/EmulatorCoreLinux/build.sh
+++ X16Emulator/EmulatorCoreLinux/build.sh
@@ -1,2 +1,2 @@
 gcc --version
-gcc -shared -fno-pie -o EmulatorCore.so -fPIC core.cpp core.obj -Wall -g -m64 -z noexecstack
+gcc -shared -fno-pie -o EmulatorCore.so -fPIC core.cpp core.obj -Wall -m64 -z noexecstack
